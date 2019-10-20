c3c7496d54b4c184f88cd2eec3b4243cd350fe0f
JonRudnick <Jon@Rudnick.org>
2019-10-20 16:23:27 -0500

    Incorporate changes from vincchan's Pull Request xlexi/alientube#182 "added German translation"

--- TypeScript/LocalisationManager.ts
+++ TypeScript/LocalisationManager.ts
@@ -14,6 +14,7 @@ module AlienTube {
     export class LocalisationManager {
         private localisationData: any;
         private supportedLocalisations = [
+            'de',
             'en',
             'en-US',
             'no',
